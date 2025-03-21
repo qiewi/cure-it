aa5e478082108b160a0e525912f9d7c4671b958b
qiewi <105619444+qiewi@users.noreply.github.com>
2025-03-21 09:49:20 +0700

    feat: added lihat selengkapnya button

--- src/components/hero/PopularHospitalsSection.tsx
+++ src/components/hero/PopularHospitalsSection.tsx
@@ -3,6 +3,8 @@
 import { useState, useEffect } from "react"
 import HospitalCard from "@/components/ui/HospitalCard"
 import { useMediaQuery } from "@/hooks/use-mobile"
+import Link from "next/link"
+import { ChevronRight } from "lucide-react"
 
 interface Hospital {
   id: number
@@ -31,9 +33,14 @@ export function PopularHospitalsSection({ hospitals }: PopularHospitalsSectionPr
   return (
     <section className="mb-6 px-4 md:px-12 mt-8 md:mt-12">
       <h2 className="text-xl font-bold mb-1 md:text-3xl">Rumah Sakit Terpopuler</h2>
-      <p className="text-xs text-neutral-600 mb-3 md:text-lg">
-        Berikut adalah beberapa rumah sakit yang kami rekomendasikan untuk Anda!
-      </p>
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-3">
+        <p className="text-xs text-neutral-600 mb-3 md:text-lg">
+          Berikut adalah beberapa rumah sakit yang kami rekomendasikan untuk Anda!
+        </p>
+        <Link href="/search/hospital" className="text-xs md:text-sm flex items-center mr-4">
+          Lihat selengkapnya <ChevronRight className="h-4 w-4" />
+        </Link>
+      </div>
 
       <div className="flex flex-col md:flex-row gap-3 md:gap-6 md:mt-8">
         {hospitals.map((hospital) => (
