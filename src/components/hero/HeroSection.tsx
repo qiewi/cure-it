9bbea28e8f13ec4ad94200d40146e70e8bd78354
qiewi <105619444+qiewi@users.noreply.github.com>
2025-03-23 22:38:32 +0700

    feat: new hero banner

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -5,7 +5,7 @@ import Small from "@Images/hero-promotion-sm.svg"
 export function HeroSection() {
   return (
     <div>
-      <div className="relative w-full hidden md:block md:h-[400px] bg-primary-100">
+      <div className="relative w-full hidden md:block md:h-[360px] bg-primary-100">
         <Image src={Medium} alt="Healthcare Hero" fill className="object-cover" priority />
       </div>
       <div className="relative w-full md:hidden h-[360px] bg-primary-100">
